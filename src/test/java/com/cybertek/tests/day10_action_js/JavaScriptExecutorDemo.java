621e582f5627c9e52ecedfb76cdc9f62a78a849c
ahmetokcu <ahmetokcu2020@gmail.com>
2022-01-05 00:12:32 +0100

    OdeV

---
+++ src/test/java/com/cybertek/tests/day10_action_js/JavaScriptExecutorDemo.java
@@ -0,0 +1,4 @@
+package com.cybertek.tests.day10_action_js;
+
+public class JavaScriptExecutorDemo {
+}
---
+++ src/test/java/com/cybertek/tests/day9_popups_tabs_frames/iframTest.java
@@ -0,0 +1,116 @@
+package com.cybertek.tests.day9_popups_tabs_frames;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class iframTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = WebDriverFactory.getDriver("chrome");
+
+    }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.quit();
+
+    }
+
+    @Test
+    public void test1() throws InterruptedException {
+        driver.get("http://practice.cybertekschool.com/iframe");
+
+        //How to switch frames
+        //1.Switch using by name or ID attribute of iframe
+        driver.switchTo().frame("mce_0_ifr");
+        Thread.sleep(2000);
+
+        //clear before sendKeys
+        driver.findElement(By.cssSelector("#tinymce")).clear();
+
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("#tinymce")).sendKeys("MikeSmith");
+
+        //goes back to first frame(main html)
+        //goes back to first frame, useful when we have swtched multiple frames
+        Thread.sleep(2000);
+        driver.switchTo().defaultContent();
+        Thread.sleep(2000);
+
+        //2.Switching with Index
+        driver.switchTo().frame(0);
+
+        //clear before sendKeys
+        driver.findElement(By.cssSelector("#tinymce")).clear();
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("#tinymce")).sendKeys("MikeSmith WITH INDEX");
+
+        //second way to switch parent
+        driver.switchTo().parentFrame();
+
+        //3.Using Webelement
+        //locating iframe with any valid locator
+        WebElement iframeElement = driver.findElement(By.tagName("iframe"));
+
+        driver.switchTo().frame(iframeElement);
+
+        Thread.sleep(2000);
+        driver.findElement(By.cssSelector("#tinymce")).sendKeys("MikeSmith WITH WEBELEMENT");
+
+
+    }
+
+    @Test
+    public void test2(){
+        driver.get("http://practice.cybertekschool.com/nested_frames");
+
+        //switching to frame top
+        driver.switchTo().frame("frame-top");
+
+        //HTML (Default Content)
+            //frame-top (parent frame)
+            //0 left
+            //1 middle
+            //2 right
+        //frame-bottom
+
+        //top has 3 frame under left middle and right
+        //switch to frame middle
+        driver.switchTo().frame("frame-middle");
+
+        System.out.println(driver.findElement(By.id("content")).getText());
+
+        //goes back to top frame
+        driver.switchTo().parentFrame();
+
+        //switchiing right with Index
+        driver.switchTo().frame(2);
+
+        System.out.println(driver.findElement(By.tagName("body")).getText());
+
+        //go to main html to switch bottom
+        driver.switchTo().defaultContent();
+
+        driver.switchTo().frame(1);
+
+        System.out.println(driver.findElement(By.tagName("body")).getText());
+
+
+
+    }
+
+
+
+}
+
+
+

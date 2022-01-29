4749fa29eb354a4318fe47d18125588c44c973b4
kavya tolety <ahmetokcu2020@gmail.com>
2022-01-30 00:22:09 +0100

    Examples

---
+++ src/test/java/com/cybertek/tests/day16_ddf/DDFLoginTest.java
@@ -0,0 +1,41 @@
+package com.cybertek.tests.day16_ddf;
+
+import com.cybertek.pages.DashboardPage;
+import com.cybertek.pages.LoginPage;
+import com.cybertek.utilities.ExcelUtil;
+import com.cybertek.utilities.TestBase;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class DDFLoginTest extends TestBase {
+
+    @DataProvider
+    public Object [][] userData(){
+
+        ExcelUtil qa3short = new ExcelUtil("src/test/resources/Vytracktestdata.xlsx","QA2-short");
+
+        String [][] dataArray =qa3short.getDataArrayWithoutFirstRow();
+
+        return dataArray;
+    }
+
+    @Test(dataProvider = "userData")
+    public void test1(String username,String password,String firstName,String lastName,String result){
+        extentLogger=report.createTest("Test "+firstName+" "+lastName);
+        LoginPage loginPage = new LoginPage();
+
+        loginPage.login(username,password);
+
+        DashboardPage dashboardPage = new DashboardPage();
+        dashboardPage.waitUntilLoaderScreenDisappear();
+
+        String actualFullName = dashboardPage.getUserName();
+        String expectedFullName = firstName+" "+lastName;
+
+        Assert.assertEquals(actualFullName,expectedFullName,"verify fullname");
+        extentLogger.pass("PASSED");
+    }
+
+
+}
